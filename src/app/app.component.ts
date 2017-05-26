2d6e8b45448a22e3f5e5173ce663c626ccd551ea
unknown <o.htiti@brams.com>
2017-05-26 09:11:20 +0100

    change project directory

---
+++ src/app/app.component.ts
@@ -0,0 +1,18 @@
+﻿import { Component } from '@angular/core';
+
+declare var jQuery: any;
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+    title = 'app works!';
+
+
+
+    doQuery() {
+        jQuery('#hello').html("Hi jquery");
+    }
+}
